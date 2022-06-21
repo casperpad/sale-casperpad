9d09539a471f5a9bb9befe2598c7a508c9d4746a
gyroflaw <ryo.kanazawa34@gmail.com>
2022-06-21 22:48:11 +0900

    feat: Remove ropsten network

--- src/config/binance.js
+++ src/config/binance.js
@@ -1,6 +1,8 @@
 const binanceProjects = {
   56: [],
-  97: [{ address: "0x1234", isBuyOnly: true }],
+  97: [
+    { address: "0xD9A3DC054057007d39330E21C88780bc754B8c56", isBuyOnly: true },
+  ],
   3: [
     { address: "0x56025BEA110Ac4283ecef59dB7279758F36e74C2", isBuyOnly: true },
   ],
