cdca0905a4fba31db4fd2eafa7a2fd0a3c3a3825
gyroflaw <ryo.kanazawa34@gmail.com>
2022-06-21 23:02:18 +0900

    fix: Project details and remove ropsten rubbishes

--- src/config/binance.js
+++ src/config/binance.js
@@ -3,9 +3,6 @@ const binanceProjects = {
   97: [
     { address: "0xD9A3DC054057007d39330E21C88780bc754B8c56", isBuyOnly: true },
   ],
-  3: [
-    { address: "0x56025BEA110Ac4283ecef59dB7279758F36e74C2", isBuyOnly: true },
-  ],
 };
 
 export const CHAIN_ID = parseInt(process.env.REACT_APP_BSC_CHAIN_ID) || 97;
