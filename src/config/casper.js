07e5f6ee971884b34becf5dcf6a086b2649e9f9c
codemaster034 <codemaster034@gmail.com>
2022-06-30 18:42:20 +0900

    feat: Add new IDO

--- src/config/casper.js
+++ src/config/casper.js
@@ -7,16 +7,17 @@ export const casperProjects = {
     },
     {
       address:
-        "4ab20dc9b5ec22663a04454009d8ceb45bd628d0bb242550b76a4234d974be78",
-      isPublic: true,
-    },
-    {
-      address:
-        "ce9235335cfae339839d4c4dad8a62ec573e44cad9634eca8f20c67f0356ef27",
+        "6fe3d97d5b5fcfb3890f470f6da71439eaf0bd3ba0e3cb4ab043cee414b6de11",
       isPublic: true,
     },
   ],
-  casper: ["74764b918573d0572fe949c434c295204d4d4afa70e067b9bb21082306da8e87"],
+  casper: [
+    {
+      address:
+        "74764b918573d0572fe949c434c295204d4d4afa70e067b9bb21082306da8e87",
+      isPublic: false,
+    },
+  ],
 };
 
 export const CASPER_CHAIN =
