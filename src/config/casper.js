fab9f91f5b71eb74300a5528fb51197a914d7a8a
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2022-09-08 22:00:24 +0900

    fix: Mainnet smartcontract hash

--- src/config/casper.js
+++ src/config/casper.js
@@ -6,6 +6,18 @@ export const casperProjects = {
       isPublic: false,
     },
   ],
+  casper: [
+    {
+      address:
+        "6cc90635ccaf0fac5c08f0533ea85336f2975202f5d01a5a1894f6f6cb306827",
+      isPublic: false,
+    },
+    // {
+    //   address:
+    //     "365eada56830d84afb71bce8385249873ac0a0e3105ddda4714427914b1280fb",
+    //   isPublic: true,
+    // },
+  ],
 };
 
 export const CASPER_CHAIN =
